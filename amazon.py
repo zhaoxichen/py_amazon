5e35df46b39660b1b0fc732d91fdd443c5eea26a
zhaoxi <458839073@qq.com>
2018-06-12 18:04:13 +0800

    vv

--- amazon.py
+++ amazon.py
@@ -7,7 +7,10 @@ Project:使用chrome浏览器，安装chromewebdriver.exe
 import time
 import re
 import os
+
+import xlrd
 import xlsxwriter
+from xlutils.copy import copy
 from selenium import webdriver
 
 
@@ -34,7 +37,7 @@ class Amazon():
             if self.my_title == title:
                 print('我们的商品可以排在第' + str(count_orders) + '位>>>> ' + title)
                 is_find = 1
-                break
+                return count_orders
 
     def product_search(self, key):
         driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]').clear()
@@ -84,18 +87,6 @@ class Amazon():
             finally:
                 rf.close()
 
-    def create_excel_file(self):
-        # 创建一个Excel文件
-        today = time.strftime('%Y%m%d%H%M', time.localtime())
-        workbook = xlsxwriter.Workbook('../excel/' + today + '.xlsx')
-        # 创建一个工作表
-        worksheet = workbook.add_worksheet()
-        # 写入第一行
-        first = ['商品名称', '品牌', '详情页网址', '原价格', '星级', '图片', '图片网址']
-        for i in range(0, len(first)):
-            worksheet.write(0, i, first[i])
-        # workbook.close()
-
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
@@ -107,9 +98,37 @@ if __name__ == '__main__':
     print('输入数字1或直接输入要查询的关键字，进入单个查询模式')
     print('输入数字2，进入多个轮询；输入英文exit，退出脚本')
     print('输入数字3,进入更换查询对象，更改商品标题')
-    print('输入英文exit，退出脚本')
+    print('输入数字88，退出脚本')
+    # 创建一个Excel文件
+    today = time.strftime('%Y%m%d%H%M', time.localtime())
+    file_name = '../excel/' + today + '.xls'
+    file_dir = os.path.split(file_name)[0]
+    # 判断文件路径是否存在，如果不存在，则创建，此处是创建多级目录
+    if not os.path.isdir(file_dir):
+        os.makedirs(file_dir)
+    # 然后再判断文件是否存在，如果不存在，则创建
+    if not os.path.exists(file_name):
+        os.system(r'touch %s' % file_name)
+    # 创建一个Excel文件
+    workbook = xlsxwriter.Workbook(file_name)
+    # 创建一个工作表
+    worksheet = workbook.add_worksheet()
+    # 写入第标题行
+    first = ['关键词', '页码', '位置', '图片', '图片网址', '商品标题']
+    for i in range(0, len(first)):
+        worksheet.write(0, i, first[i])
+    workbook.close()  # 关闭
+    row = 1
     # 无限循环
     while 1:
+        # 打开exce文件,可追加写入
+        # pip install xlrd
+        # pip install xlwt
+        # pip install xlutils
+        rexcel = xlrd.open_workbook(file_name)  # 用wlrd提供的方法读取一个excel文件，保持原有格式
+        rows = rexcel.sheets()[0].nrows  # 用wlrd提供的方法获得现在已有的行数
+        excel = copy(rexcel)  # 用xlutils提供的copy方法将xlrd的对象转化为xlwt的对象
+        table = excel.get_sheet(0)  # 用xlwt对象的方法获得要操作的sheet
         # 判断要做的事情
         print('******************************************************************************')
         arm_handle = input('请指示模式或直接输入要查询的keyword，回车确认：')
@@ -124,7 +143,8 @@ if __name__ == '__main__':
         elif '3' == arm_handle:
             tile_in = input('更换要查询的商品标题，请输入，回车确认：')
             my_amazon.change_self_title(tile_in)
-        elif 'exit' == arm_handle:
+            continue
+        elif '88' == arm_handle:
             # 退出
             print('***************************************谢谢支持！！！***************************************')
             break
@@ -132,7 +152,9 @@ if __name__ == '__main__':
             # 默认是单个关键字查询模式
             key_arr.append(arm_handle)
             print(key_arr)
-
+        arr_keys_find = []
+        arr_page = []
+        arr_pos = []
         for my_key in key_arr:
             # 输入，搜索
             print('*********************************************************************************************')
@@ -150,8 +172,11 @@ if __name__ == '__main__':
                 counter += 1
                 print("第" + str(counter) + '页')
                 # 获取位置
-                my_amazon.get_product_position_title()
+                position_orders = my_amazon.get_product_position_title()
                 if 1 == is_find:
+                    arr_keys_find.append(my_key)
+                    arr_page.append(counter)
+                    arr_pos.append(position_orders)
                     break
                 else:
                     print('没有发现我们的商品')
@@ -164,6 +189,22 @@ if __name__ == '__main__':
                 except:
                     print('翻完了所有页码都没有找到,这都写的什么关键字呀！！！！')
                     break
+        # 向excel写入数据
+        for j in range(0, len(arr_page)):
+            table.write(row, 0, arr_keys_find[j])
+            table.write(row, 1, arr_page[j])
+            table.write(row, 2, arr_pos[j])
+            table.write(row, 5, tile_in)
+            row = row + 1
+        try:
+            excel.save(file_name)
+        except:
+            print('数据写入失败，请不要打开当前excel文件')
+            input('输入数字1，按回车重试一次:')
+            try:
+                excel.save(file_name)
+            except:
+                print('数据写入失败，抱歉！')
         print('***************************************本次查询工作完成***************************************')
     # 关闭浏览器
     driver.quit()
