a262e5b343edafad9d6d7f2aa6f7f36625f5f360
zhaoxi <458839073@qq.com>
2018-06-22 18:29:02 +0800

    vv

--- amazon_key_ranking.py
+++ amazon_key_ranking.py
@@ -39,34 +39,36 @@ def read_file(file_name):
 if __name__ == "__main__":
     my_asin = 'B07964CWVK'
     keywords = 'dogs'
-    urldata = {
-        'rh': 'i:aps,k:' + keywords,
-        'field-keywords': keywords,
-        'page': '2'
-    }
-    urldata = urllib.parse.urlencode(urldata)
-    url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&" + urldata
-    # 第一步 取出页面的数据
-    resp = requests.get(url=url, headers=unicornHeader, stream=True)
-    respContent = resp.content
-    # print(f"respText = {respText}")
+    for page in range(0, 10):
+        urldata = {
+            'rh': 'i:aps,k:' + keywords,
+            'field-keywords': keywords,
+            'page': page
+        }
+        urldata = urllib.parse.urlencode(urldata)
+        url = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&" + urldata
+        # 第一步 取出页面的数据
+        resp = requests.get(url=url, headers=unicornHeader, stream=True)
+        respContent = resp.content
+        # print(f"respText = {respText}")
 
-    # 第二步：从主页中提取出相关的参数
-    # xpath解析需要的东西
-    html = etree.HTML(respContent)
-    # print(etree.tostring(html))
-    print('-----------------------------------------------------------------------------------------')
-    # //*[@id="s-results-list-atf"]
-    print('***************' + keywords + '***************')
-    ranking = 0
-    # result_titles = html.xpath('//*[@id="s-results-list-atf"]/li/@data-asin')
-    result_titles = html.xpath('//li[@data-asin]')
-    for result_title in result_titles:
-        result_id = result_title.get('id')
-        result_asin_code = result_title.get('data-asin')
-        result_class = result_title.get('class')
-        print(result_id + '>>>' + result_asin_code + '>>>' + result_class)
-        if my_asin == result_asin_code:
-            print('我的商品排在>>>' + str(ranking))
-            break
-        ranking = ranking + 1
+        # 第二步：从主页中提取出相关的参数
+        # xpath解析需要的东西
+        html = etree.HTML(respContent)
+        # print(etree.tostring(html))
+        print('***************' + keywords + '***************')
+        ranking = 0
+        # result_titles = html.xpath('//*[@id="s-results-list-atf"]/li/@data-asin')
+        result_titles = html.xpath('//li[@data-asin]')
+        print('这一页有' + str(len(result_titles)) + '个商品')
+        for result_title in result_titles:
+            result_id = result_title.get('id')
+            result_asin_code = result_title.get('data-asin')
+            result_class = result_title.get('class')
+            print(result_id + '>>>' + result_asin_code + '>>>' + result_class)
+            if my_asin == result_asin_code:
+                print('我的商品排在>>>' + str(ranking))
+                break
+            ranking = ranking + 1
+        page = page + 1
+        print('*********************************************************************')
