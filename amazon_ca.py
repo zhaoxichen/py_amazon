3d803a148143e2eb65c2aebf50e25b0552714f3b
zhaoxi <458839073@qq.com>
2018-06-22 15:21:34 +0800

    vv

---
+++ amazon_ca.py
@@ -0,0 +1,235 @@
+# coding=utf-8
+'''
+Created on 2016-8-16
+@author: Jennifer
+Project:使用chrome浏览器，安装chromewebdriver.exe
+'''
+import time
+import re
+import os
+
+import xlrd
+import xlsxwriter
+from xlutils.copy import copy
+from selenium import webdriver
+
+
+class Amazon():
+
+    def __init__(self):
+        # 商品标题
+        self.my_title = '125ml Pet Drinking Bottle with Food Container Base Hanging Water Feeding Bottles Auto Dispenser for Hamsters Rats Small Animals Ferrets Rabbits Small Animals (125ML, Blue)'
+
+    def change_self_title(self, title_in):
+        self.my_title = title_in
+
+    def get_product_position_title(self):
+        global is_find
+        # 查看商品标题
+        product_list_title = driver.find_elements_by_xpath('//*[@id="s-results-list-atf"]/li/div/div//div[1]/a/h2')
+        print('本页共有' + str(len(product_list_title)) + '个商品，', end='')
+        count_orders = 0
+        for title_h2 in product_list_title:
+            count_orders += 1
+            title = title_h2.get_attribute('data-attribute')
+            if self.my_title == title:
+                print('我们的商品可以排在第' + str(count_orders) + '位>>>> ' + title)
+                is_find = 1
+                return count_orders
+
+    def product_search(self, key):
+        driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]').clear()
+        driver.find_element_by_xpath('//*[@id="twotabsearchtextbox"]').send_keys(key)
+        driver.find_element_by_xpath('//*[@id="nav-search"]/form/div[2]/div/input').click()
+
+    # 元素存在，且唯一
+    def is_element_exist_unique(self, css_str):
+        s = driver.find_elements_by_css_selector(css_selector=css_str)
+        if len(s) == 0:
+            print
+            "元素未找到:%s" % css_str
+            return False
+        elif len(s) == 1:
+            return True
+        else:
+            print
+            "找到%s个元素：%s" % (len(s), css_str)
+            return False
+
+    # 元素存在
+    def is_element_exist(self, css_str):
+        try:
+            driver.find_element_by_css_selector(css_str)
+            return True
+        except:
+            return False
+
+    # 读文件
+    def read_file(self, file_name):
+        if os.path.exists(file_name):
+            print(file_name)
+        else:
+            file_dir = os.path.split(file_name)[0]
+            # 判断文件路径是否存在，如果不存在，则创建，此处是创建多级目录
+            if not os.path.isdir(file_dir):
+                os.makedirs(file_dir)
+            file = open(file_name, 'w')
+            file.close()
+        string = ''
+        with open(file_name, "r+", encoding='utf-8') as rf:
+            try:
+                # 读每行
+                list_of_all_the_lines = rf.readlines()
+                for one_line in list_of_all_the_lines:
+                    string += one_line
+                rf.close()
+                return string
+            finally:
+                rf.close()
+
+
+if __name__ == '__main__':
+    my_amazon = Amazon()
+    tile_in = input('请输入要查询的商品标题，回车确认：')
+    if '2' == tile_in:
+        my_title_file = '../res_input/my_title.txt'
+        tile_in = keys_str = my_amazon.read_file(my_title_file)
+    if '' == tile_in:
+        print(my_amazon.my_title)
+    else:
+        my_amazon.change_self_title(tile_in)
+    print('查询的商品>>>' + tile_in)
+    # 创建一个Excel文件
+    today = time.strftime('%Y%m%d%H%M', time.localtime())
+    file_name = '../excel/' + today + '.xls'
+    file_dir = os.path.split(file_name)[0]
+    # 判断文件路径是否存在，如果不存在，则创建，此处是创建多级目录
+    if not os.path.isdir(file_dir):
+        os.makedirs(file_dir)
+    # 然后再判断文件是否存在，如果不存在，则创建
+    if not os.path.exists(file_name):
+        file = open(file_name, 'w')
+        file.close()
+    # 创建一个Excel文件
+    workbook = xlsxwriter.Workbook(file_name)
+    # 创建一个工作表
+    worksheet = workbook.add_worksheet()
+    # 写入第标题行
+    first = ['关键词', '页码', '位置', '图片', '图片网址', '商品标题']
+    for i in range(0, len(first)):
+        worksheet.write(0, i, first[i])
+    workbook.close()  # 关闭
+    row = 1
+    # 打开浏览器
+    driver = webdriver.Chrome()
+    driver.get('https://www.amazon.ca')
+    print('加拿大站点脚本使用说明：')
+    print('输入数字1或直接输入要查询的关键字，进入单个查询模式')
+    print('输入数字2，进入多个轮询；输入英文exit，退出脚本')
+    print('输入数字3,进入更换查询对象，更改商品标题')
+    print('输入数字88，退出脚本')
+    # 无限循环
+    while 1:
+        # 打开exce文件,可追加写入
+        # pip install xlrd
+        # pip install xlwt
+        # pip install xlutils
+        rexcel = xlrd.open_workbook(file_name)  # 用wlrd提供的方法读取一个excel文件
+        rows = rexcel.sheets()[0].nrows  # 用wlrd提供的方法获得现在已有的行数
+        excel = copy(rexcel)  # 用xlutils提供的copy方法将xlrd的对象转化为xlwt的对象
+        table = excel.get_sheet(0)  # 用xlwt对象的方法获得要操作的sheet
+        # 判断要做的事情
+        print('******************************************************************************')
+        arm_handle = input('请指示模式或直接输入要查询的keyword，回车确认：')
+        key_arr = []
+        if '1' == arm_handle:
+            key_in = input('请输入要查询的关键字，回车确认:')
+            key_arr.append(key_in)
+            print(key_arr)
+        elif '2' == arm_handle:
+            my_keys_file = '../res_input/keywords.txt'
+            keys_str = my_amazon.read_file(my_keys_file)
+            if '' == keys_str:
+                print('文本中没有预设关键词')
+            else:
+                key_arr = re.split('[,]', keys_str)
+        elif '3' == arm_handle:
+            tile_in = input('更换要查询的商品标题，请输入，回车确认：')
+            my_amazon.change_self_title(tile_in)
+            if '2' == tile_in:
+                tile_in = keys_str = my_amazon.read_file(my_title_file)
+            if '' == tile_in:
+                print(my_amazon.my_title)
+            else:
+                my_amazon.change_self_title(tile_in)
+            print('查询的商品>>>' + tile_in)
+            continue
+        elif '88' == arm_handle:
+            # 退出
+            print('***************************************谢谢支持！！！***************************************')
+            break
+        elif '' == arm_handle:
+            key_arr.append('hamster water bottle with base')
+        else:
+            # 默认是单个关键字查询模式
+            key_arr.append(arm_handle)
+            print(key_arr)
+        arr_keys_find = []
+        arr_page = []
+        arr_pos = []
+        for my_key in key_arr:
+            # 输入，搜索
+            print('*********************************************************************************************')
+            print('输入关键字>>> ' + my_key)
+            my_amazon.product_search(my_key)
+            # 搜索需要加载时间
+            time.sleep(3)
+            counter = 0
+            is_find = 0
+            # 开始查询
+            while 0 == is_find:
+                if counter > 9:
+                    print('翻完到了第十页都没有找到,这都写的什么关键字呀！不翻了')
+                    break
+                counter += 1
+                print("第" + str(counter) + '页')
+                # 获取位置
+                position_orders = my_amazon.get_product_position_title()
+                if 1 == is_find:
+                    arr_keys_find.append(my_key)
+                    arr_page.append(counter)
+                    arr_pos.append(position_orders)
+                    break
+                else:
+                    print('没有发现我们的商品')
+                    print('点击下一页...')
+                try:
+                    next_page = driver.find_element_by_id('pagnNextString')
+                    next_page.click()
+                    # 翻页加载需要时间
+                    time.sleep(3)
+                except:
+                    print('翻完了所有页码都没有找到,这都写的什么关键字呀！！！！')
+                    break
+        # 向excel写入数据
+        for j in range(0, len(arr_page)):
+            table.write(row, 0, arr_keys_find[j])
+            table.write(row, 1, arr_page[j])
+            table.write(row, 2, arr_pos[j])
+            table.write(row, 5, tile_in)
+            row = row + 1
+        try:
+            excel.save(file_name)
+        except:
+            print('***************************************数据写入失败***************************************')
+            print('数据写入失败，请不要打开当前excel文件，请关闭当前正在操作的excel后，输入数字1，回车重试一次')
+            print('***************************************数据写入失败***************************************')
+            input('输入数字1，按回车重试一次:')
+            try:
+                excel.save(file_name)
+            except:
+                print('数据写入失败，抱歉！')
+        print('***************************************本次查询工作完成***************************************')
+    # 关闭浏览器
+    driver.quit()
+    print('***************************************再见！！！***************************************')
---
+++ amazon_ca_key_completion.py
@@ -0,0 +1,72 @@
+import requests
+import re
+
+unicornHeader = {
+    'Host': 'completion.amazon.com',
+    'Referer': 'https://www.amazon.ca',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+}
+
+if __name__ == "__main__":
+    # 第一步：先把amazon主页拉下来
+    resp = requests.get("https://www.amazon.ca/ref=nav_logo", headers=unicornHeader)
+    respText = resp.text
+    # print(f"respText = {respText}")
+
+    # 第二步：从主页中提取出相关的参数
+    '''
+        method:completion
+        mkt:1
+        r:T9GNBFENMKCSHQ96SN69
+        s:136-4489048-3064812
+        c:
+        p:Gateway
+        l:en_US
+        b2b:0
+        fresh:0
+        sv:desktop
+        client:amazon-search-ui
+        x:String
+        search-alias:mobile
+        ks:82
+        q:car
+        qs:
+        cf:1
+        fb:1
+        sc:1
+    '''
+    # r:T9GNBFENMKCSHQ96SN69
+    # var ue_id = 'T9GNBFENMKCSHQ96SN69',
+    ue_id_Re = re.search("ue_id = '(.*?)'", respText, re.DOTALL)
+    print(f"ue_id_Re = {ue_id_Re}")
+    if ue_id_Re:
+        ue_id = ue_id_Re.group(1)
+    else:
+        ue_id = ""
+
+    # s:136-4489048-3064812
+    # ue_sid = '136-4489048-3064812',
+    ue_sid_Re = re.search("ue_sid = '(.*?)'", respText, re.DOTALL)
+    print(f"ue_sid_Re = {ue_sid_Re}")
+    if ue_sid_Re:
+        ue_sid = ue_sid_Re.group(1)
+    else:
+        ue_sid = ""
+
+    while 1:
+        # 第三步：构造请求
+        # 原始关键字
+        originalKey = input('请输入要查询的原始关键词（回车）：')
+        if originalKey == '88':
+            break
+        # search_alias = 'mobile'
+        # aps                       # All Departments 类别
+        search_alias = 'aps'  # 搜索类别，这个是类别下拉框中的。主页源码中也有
+        ks = 100  # 先自己指定，发现并不影响结果
+        # https://completion.amazon.com/search/complete?method=completion&mkt=7&r=MCDQ2E39H2C182CM74N1&s=135-9647165-5161406&c=&p=Gateway&l=en_CA&b2b=0&fresh=0&sv=desktop&client=amazon-search-ui&x=String&search-alias=aps&ks=8&q=do&qs=&cf=1&fb=1&sc=1&
+        keywordsUrl = f'https://completion.amazon.com/search/complete?method=completion&mkt=7&r={ue_id}&s={ue_sid}c=&p=Gateway&l=en_CA&b2b=0&fresh=0&sv=desktop&client=amazon-search-ui&x=String&search-alias={search_alias}&ks={ks}&q={originalKey}&qs=&cf=1&fb=1&sc=1&'
+        secondResp = requests.get(keywordsUrl, headers=unicornHeader)
+        print('*****************************************************************************************')
+        print('原始关键词>>>' + originalKey)
+        print('******************************按热度补全后的关键词列表如下****************************************')
+        print(secondResp.text)
